960600a56dd62d78bf869f818f4a978553125d2d
zhangsanshi <zhangsanshi5566@gmail.com>
2020-05-14 22:08:40 +0800

    fix: isMicroApp errors

--- dist/isMicro.js
+++ dist/isMicro.js
@@ -1,7 +1,8 @@
 var isMicroApp = function (appName) {
-    return Object.values(window.micro.config).some(function (app) { return app.name === appName; });
+    return Object.values(window.micro.config).some(function (apps) { return apps.some(function (app) { return app.name === appName; }); }) || !!window.micro.subApps[appName];
 };
 var micro = window.micro = (window.micro || {});
 micro.config = micro.config || {};
+micro.subApps = micro.subApps || {};
 var isMicro = MICRO_NAME && isMicroApp(MICRO_NAME);
 export default isMicro;
--- src/isMicro.ts
+++ src/isMicro.ts
@@ -7,14 +7,21 @@ declare global {
                     [prop: string]: any;
                 };
             };
+            subApps: {
+                [prop: string]: {
+                    name: string;
+                    [prop: string]: any;
+                };
+            };
         };
     }
 }
 const isMicroApp = function (appName): boolean {
-    return Object.values(window.micro.config).some((app) => app.name === appName);
+    return Object.values(window.micro.config).some((apps) => apps.some((app) => app.name === appName)) || !!window.micro.subApps[appName];
 };
 
 const micro = window.micro = (window.micro || {}) as typeof window.micro;
 micro.config = micro.config || {};
+micro.subApps = micro.subApps || {};
 const isMicro = MICRO_NAME && isMicroApp(MICRO_NAME);
 export default isMicro;
